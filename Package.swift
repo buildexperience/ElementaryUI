d5f241f304441044c103a7f587af53b08e3959ad
Joe Maghzal <joemaghzal@icloud.com>
2024-02-27 18:58:25 +0200

    Initial commit

--- Package.swift
+++ Package.swift
@@ -1,23 +1,16 @@
 // swift-tools-version: 5.9
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "NJKit",
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "NJKit",
-            targets: ["NJKit"]),
+        .library(name: "NJKit", targets: ["NJKit"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "NJKit"),
-        .testTarget(
-            name: "NJKitTests",
-            dependencies: ["NJKit"]),
+        .target(name: "NJKit"),
+        .testTarget(name: "NJKitTests", dependencies: [
+            "NJKit"
+        ]),
     ]
 )
